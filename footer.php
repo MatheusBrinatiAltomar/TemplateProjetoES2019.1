fd75f690db256972569e849428af25685302f13d
Matheus Brinati Altomar <mthaltomar@gmail.com>
2019-02-05 11:52:18 -0200

    Identidade Visual + Bootstrap footer/header

--- footer.php
+++ footer.php
@@ -1,3 +1,135 @@
-<footer>
-    <h2>Aqui está o Footer!</h2>
-</footer>
+
+<!-- Footer -->
+<footer class="page-footer font-small mdb-color pt-4">
+
+    <!-- Footer Links -->
+    <div class="container text-center text-md-left">
+
+      <!-- Footer links -->
+      <div class="row text-center text-md-left mt-3 pb-3">
+
+        <!-- Coluna no Grid -->
+        <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+          <h6 class="text-uppercase mb-4 font-weight-bold">Company name</h6>
+          <p>Aqui você pode usar linhas e colunas para organizar seu rodapé. Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit.</p>
+        </div>
+        <!-- Coluna no Grid -->
+
+        <hr class="w-100 clearfix d-md-none">
+
+        <!-- Coluna no Grid -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+          <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
+          <p>
+            <a href="#!">MDBootstrap</a>
+          </p>
+          <p>
+            <a href="#!">MDWordPress</a>
+          </p>
+          <p>
+            <a href="#!">BrandFlow</a>
+          </p>
+          <p>
+            <a href="#!">Bootstrap Angular</a>
+          </p>
+        </div>
+        <!-- Coluna no Grid -->
+
+        <hr class="w-100 clearfix d-md-none">
+
+        <!-- Coluna no Grid -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+          <h6 class="text-uppercase mb-4 font-weight-bold">Useful links</h6>
+          <p>
+            <a href="#!">Your Account</a>
+          </p>
+          <p>
+            <a href="#!">Become an Affiliate</a>
+          </p>
+          <p>
+            <a href="#!">Shipping Rates</a>
+          </p>
+          <p>
+            <a href="#!">Help</a>
+          </p>
+        </div>
+
+        <!-- Coluna no Grid -->
+        <hr class="w-100 clearfix d-md-none">
+
+        <!-- Coluna no Grid -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+          <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+          <p>
+            <i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
+          <p>
+            <i class="fas fa-envelope mr-3"></i> info@gmail.com</p>
+          <p>
+            <i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+          <p>
+            <i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+        </div>
+        <!-- Coluna no Grid -->
+
+      </div>
+      <!-- Footer links -->
+
+      <hr>
+
+      <!-- Linha no Grid -->
+      <div class="row d-flex align-items-center">
+
+        <!-- Coluna no Grid -->
+        <div class="col-md-7 col-lg-8">
+
+          <!--Copyright-->
+          <p class="text-center text-md-left">© 2018 Copyright:
+            <a href="https://mdbootstrap.com/education/bootstrap/">
+              <strong> MDBootstrap.com</strong>
+            </a>
+          </p>
+
+        </div>
+        <!-- Coluna no Grid -->
+
+        <!-- Coluna no Grid -->
+        <div class="col-md-5 col-lg-4 ml-lg-0">
+
+          <!-- Social buttons -->
+          <div class="text-center text-md-right">
+            <ul class="list-unstyled list-inline">
+              <li class="list-inline-item">
+                <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                  <i class="fab fa-twitter"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                  <i class="fab fa-google-plus-g"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                  <i class="fab fa-linkedin-in"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+
+        </div>
+        <!-- Coluna no Grid -->
+
+      </div>
+      <!-- Linha no Grid -->
+
+    </div>
+    <!-- Footer Links -->
+
+  </footer>
+  <!-- Footer -->
--- index.php
+++ index.php
@@ -5,8 +5,7 @@
     <meta http-equiv="X-UA-Compatible">
     <title>Lojão do Zé</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="publico/css/estilo.css"/>
-    <script src="main.js"></script>
+    <link rel="stylesheet" href="publico/css/bootstrap.css">
 </head>
 <?php
     include "header.php";
@@ -14,11 +13,11 @@
 <body>
     <h1>Aqui está o corpo do Site!</h1>
 
+    <img src="publico/imagens/Logo_da_UFJF.png" alt="UFJF logo" class="logoufjf">
+
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nec ligula quis lorem scelerisque cursus ac non metus. Sed eleifend elementum justo at pretium. Proin et mauris ut neque tincidunt vestibulum. Nunc scelerisque, odio faucibus ullamcorper hendrerit, orci neque ullamcorper leo, ac mattis purus lectus et elit. Integer finibus faucibus arcu, eget sodales purus dignissim a. Proin semper rhoncus neque, sed auctor augue sagittis et. Aenean risus sem, commodo eu ultricies in, vestibulum sit amet nisi.</p>
 
     <p>Etiam nunc nulla, molestie vitae velit ac, bibendum eleifend libero. Proin ut rhoncus nisl, elementum auctor elit. Quisque sit amet lobortis felis. Donec lobortis massa ipsum, et congue erat congue ut. Proin sollicitudin lacus eget nibh maximus, eu dignissim arcu luctus. Phasellus tristique dapibus volutpat. Etiam scelerisque, lacus aliquet posuere maximus, eros velit sagittis metus, eget cursus lacus mauris eu odio. Aenean eu volutpat mauris, sollicitudin feugiat eros. Sed non lacus ex. Nulla accumsan porttitor turpis, elementum congue odio condimentum eu. Donec eget libero et ex dignissim pulvinar ut sed ipsum. Nulla et tincidunt enim. Nunc dictum magna non orci sollicitudin elementum.</p>
-
-    <img src="publico/imagens/produtos.jpg" class="responsive">
 
     <p>Quisque vitae augue mi. Suspendisse semper aliquam gravida. Suspendisse sodales libero lacus. Nam sed augue quam. Sed maximus metus vel tempor ultrices. Duis laoreet mi et neque ultricies, at vestibulum quam finibus. Aliquam tempor nisl enim, eu vehicula eros placerat eget. Maecenas iaculis, lorem eget malesuada pellentesque, justo nisl aliquet mi, ut sollicitudin metus nisl non lectus. Nullam ac eros et mauris commodo egestas vitae eu massa. Curabitur lorem nisl, aliquam sit amet tristique nec, lacinia at sapien. Suspendisse libero justo, facilisis at dapibus ut, iaculis quis nunc. Aliquam a elit vel eros tincidunt consequat. Etiam vel sapien sem.</p>
 
